8fb7b412a45a9de58eb65d85ad5c55d42d7c5259
Kausar Khizra <khizra6@gmail.com>
2018-10-06 13:02:39 -0700

    added the routes in the react browserRouter in index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx'
 import {Route, BrowserRouter, Switch} from 'react-router-dom'
-
+import {Crud, Search} from './components/Services.jsx'
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
+      <Route path="/search" component={Search} />
+      <Route path="/crud" component={Crud} />
+      <Route path="/versions" component={App} />
+      <Route path="/stats" component={App} />
+      <Route path="/home" component={App} />
       <Route path="/" component={App} />
     </Switch>
   </BrowserRouter>, document.getElementById('app'));
